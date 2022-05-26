a55fd64f485503fe022d4333edbebd880a0f48a0
Alex Heal <24870446+AlexHeal@users.noreply.github.com>
2022-05-26 11:39:56 +0500

    REF: move assert to page

--- src/test/java/alexheal/eshop/tests/AuthTests.java
+++ src/test/java/alexheal/eshop/tests/AuthTests.java
@@ -4,7 +4,6 @@ import alexheal.eshop.config.App;
 import alexheal.eshop.ui.HeaderSection;
 import alexheal.eshop.ui.HomePage;
 import alexheal.eshop.ui.LoginPage;
-import com.codeborne.selenide.Condition;
 import io.qameta.allure.Description;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,6 +17,6 @@ public class AuthTests extends TestBase {
         new HeaderSection().clickLogin();
         new LoginPage().authorization(App.config.userLogin(), App.config.userPassword());
 
-        new HeaderSection().userName.shouldHave(Condition.exactTextCaseSensitive(App.config.userLogin()));
+        new HeaderSection().verifyUsername(App.config.userLogin());
     }
 }
--- src/test/java/alexheal/eshop/ui/HeaderSection.java
+++ src/test/java/alexheal/eshop/ui/HeaderSection.java
@@ -1,18 +1,20 @@
 package alexheal.eshop.ui;
 
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 
 public class HeaderSection {
-    public SelenideElement userName = $(".esh-identity-name");
-
     @Step("click 'Login'")
     public HeaderSection clickLogin() {
-        $(byText("LOGIN")).click();
+        $(".esh-identity-name").click();
+
+        return this;
+    }
+
+    public HeaderSection verifyUsername(String username) {
+        $(".esh-identity-name").shouldHave(Condition.exactTextCaseSensitive(username));
 
         return this;
     }
