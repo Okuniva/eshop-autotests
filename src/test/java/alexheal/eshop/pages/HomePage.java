50e497a2c9769b3c0304d10d8a290dbb91e6ea73
Alex Heal <24870446+AlexHeal@users.noreply.github.com>
2022-05-28 13:00:47 +0500

    REF: HomePageTests as page object

--- src/test/java/alexheal/eshop/pages/HomePage.java
+++ src/test/java/alexheal/eshop/pages/HomePage.java
@@ -1,14 +1,37 @@
 package alexheal.eshop.pages;
 
 import alexheal.eshop.config.App;
+import alexheal.eshop.helpers.DriverUtils;
 import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.title;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class HomePage {
     @Step("open HomePage")
     public HomePage openHomePage() {
         open(App.config.webUrl());
+
+        return this;
+    }
+
+    @Step("Page title should have text 'eShopOnContainers - SPA'")
+    public HomePage verifyTitle() {
+        String expectedTitle = "eShopOnContainers - SPA";
+        String actualTitle = title();
+
+        assertThat(actualTitle).isEqualTo(expectedTitle);
+
+        return this;
+    }
+
+    @Step("Console logs should not contain text 'SEVERE'")
+    public HomePage verifyConsoleLog() {
+        String consoleLogs = DriverUtils.getConsoleLogs();
+        String errorText = "SEVERE";
+
+        assertThat(consoleLogs).doesNotContain(errorText);
 
         return this;
     }
--- src/test/java/alexheal/eshop/tests/HomePageTests.java
+++ src/test/java/alexheal/eshop/tests/HomePageTests.java
@@ -1,43 +1,23 @@
 package alexheal.eshop.tests;
 
-import alexheal.eshop.config.App;
-import alexheal.eshop.config.Project;
-import alexheal.eshop.helpers.DriverUtils;
-import io.qameta.allure.Description;
+import alexheal.eshop.pages.HomePage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.Selenide.title;
-import static io.qameta.allure.Allure.step;
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class HomePageTests extends TestBase {
     @Test
     @DisplayName("Page title should have header text")
     void titleTest() {
-        step("Open homepage", () ->
-                open(App.config.webUrl()));
-
-        step("Page title should have text 'eShopOnContainers - SPA'", () -> {
-            String expectedTitle = "eShopOnContainers - SPA";
-            String actualTitle = title();
-
-            assertThat(actualTitle).isEqualTo(expectedTitle);
-        });
+        new HomePage()
+                .openHomePage()
+                .verifyTitle();
     }
 
     @Test
     @DisplayName("Page console log should not have errors")
     void consoleShouldNotHaveErrorsTest() {
-        step("Open homepage", () ->
-                open(App.config.webUrl()));
-
-        step("Console logs should not contain text 'SEVERE'", () -> {
-            String consoleLogs = DriverUtils.getConsoleLogs();
-            String errorText = "SEVERE";
-
-            assertThat(consoleLogs).doesNotContain(errorText);
-        });
+        new HomePage()
+                .openHomePage()
+                .verifyConsoleLog();
     }
 }
