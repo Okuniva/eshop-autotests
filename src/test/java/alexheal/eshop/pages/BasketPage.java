576a2d83cd0c2127b1667d1b1c00e2c5a4d0387c
Alex Heal <24870446+AlexHeal@users.noreply.github.com>
2022-05-30 18:39:57 +0500

    ADD: wait header animation

--- src/test/java/alexheal/eshop/pages/BasketPage.java
+++ src/test/java/alexheal/eshop/pages/BasketPage.java
@@ -1,18 +1,24 @@
 package alexheal.eshop.pages;
 
+import com.codeborne.selenide.ClickOptions;
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 
+import static com.codeborne.selenide.Condition.cssValue;
 import static com.codeborne.selenide.Selectors.byTagAndText;
-import static com.codeborne.selenide.Selectors.withTextCaseInsensitive;
 import static com.codeborne.selenide.Selenide.$;
 
 public class BasketPage {
+    public BasketPage() {
+        $(".esh-app-header")
+                .shouldHave(cssValue("height", "208px"));
+    }
+
     @Step("click 'Checkout'")
     public BasketPage clickCheckout() {
-        $(byTagAndText("button", "Checkout")).hover().click();
+        $(byTagAndText("button", "Checkout"))
+                .hover().click(ClickOptions.usingDefaultMethod());
 
         return this;
     }
