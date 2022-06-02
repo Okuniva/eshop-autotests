f07234899e41447cd76bbd59c9fe59bf77ffec2a
Alex Heal <24870446+AlexHeal@users.noreply.github.com>
2022-06-02 16:21:38 +0500

    ADD: api getItemsTest

---
+++ src/test/java/alexheal/eshop/tests/api/Catalog.java
@@ -0,0 +1,19 @@
+package alexheal.eshop.tests.api;
+
+import org.junit.jupiter.api.Test;
+
+import static alexheal.eshop.tests.api.Specs.request;
+import static alexheal.eshop.tests.api.Specs.responseSpec;
+import static io.restassured.RestAssured.given;
+
+public class Catalog {
+    @Test
+    void getItemsTest() {
+        given()
+                .spec(request)
+                .when()
+                .get("Catalog/items?pageSize=10&pageIndex=0")
+                .then()
+                .spec(responseSpec);
+    }
+}
---
+++ src/test/java/alexheal/eshop/tests/api/Specs.java
@@ -0,0 +1,22 @@
+package alexheal.eshop.tests.api;
+
+import alexheal.eshop.config.App;
+import alexheal.eshop.helpers.AllureRestAssuredFilter;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
+import static io.restassured.RestAssured.with;
+
+public class Specs {
+    public static RequestSpecification request = with()
+            .baseUri(App.config.apiUrl())
+            .basePath("/api/v1")
+            .log().all()
+            .contentType(ContentType.JSON);
+
+    public static ResponseSpecification responseSpec = new ResponseSpecBuilder()
+            .expectStatusCode(200)
+            .build();
+}
