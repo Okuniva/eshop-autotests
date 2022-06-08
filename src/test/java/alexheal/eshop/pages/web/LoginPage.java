7917d03fca8d6a968dbc50899bd1989910f2a341
Alex Heal <24870446+AlexHeal@users.noreply.github.com>
2022-06-08 08:42:15 +0500

    REF: use @Param hidden mode

--- src/test/java/alexheal/eshop/pages/web/LoginPage.java
+++ src/test/java/alexheal/eshop/pages/web/LoginPage.java
@@ -2,15 +2,16 @@ package alexheal.eshop.pages.web;
 
 import alexheal.eshop.helpers.AllureAttachments;
 import com.codeborne.selenide.SetValueOptions;
+import io.qameta.allure.Param;
 import io.qameta.allure.Step;
+import io.qameta.allure.model.Parameter;
 
 import static com.codeborne.selenide.Selenide.$;
 
 public class LoginPage {
     @Step("authorization")
-    public LoginPage authorization(String email, String password) {
-        AllureAttachments.removeParameters();
-
+    public LoginPage authorization(@Param(mode = Parameter.Mode.HIDDEN) String email,
+                                   @Param(mode = Parameter.Mode.HIDDEN) String password) {
         $("#Email").setValue(SetValueOptions.withText(email)
                 .withDisplayedText(email.substring(0, 2) + "* - email"));
         $("#Password").setValue(SetValueOptions.withText(password).sensitive())
